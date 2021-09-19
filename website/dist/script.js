ce4fca14a3e880296e19e77d9bd9465e3cffb10a
MarkCaii <caimark93@gmail.com>
2021-09-18 20:58:33 -0400

    Added insert task form

--- website/dist/script.js
+++ website/dist/script.js
@@ -108,6 +108,26 @@ function refresh() {
   }, 800);
 }
 
+var modal = document.getElementById("taskInfoForm");
+
+var btn = document.getElementById("addTaskButton");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 $(function() {
   var err = $(".err"),
     formControl = $(".form-control"),
